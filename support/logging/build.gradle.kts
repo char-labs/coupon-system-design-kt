def33184d6433b0b6836abc360cf736e8b6e461d
ybchar <uiurihappy@naver.com>
2025-12-19 00:29:05 +0900

    [Chore] Logging, Monitoring Module

--- support/logging/build.gradle.kts
+++ support/logging/build.gradle.kts
@@ -1,1 +1,5 @@
-// Logging support module
+dependencies {
+    implementation("io.sentry:sentry-logback:${project.properties["sentryVersion"]}")
+    implementation("io.micrometer:micrometer-tracing-bridge-brave")
+    implementation("org.springframework.boot:spring-boot-starter-opentelemetry")
+}
--- support/monitoring/build.gradle.kts
+++ support/monitoring/build.gradle.kts
@@ -1,1 +1,4 @@
-// Monitoring support module
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.micrometer:micrometer-registry-prometheus")
+}
