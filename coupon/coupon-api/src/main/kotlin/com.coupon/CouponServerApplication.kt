7253819d95de67c7fd2f27a87ad585cbf5a45689
char-yb <uiurihappy@naver.com>
2026-03-26 00:54:05 +0900

    [Fix] 기본 Security 생성 비밀번호 로그 비활성화

--- coupon/coupon-api/src/main/kotlin/com.coupon/CouponServerApplication.kt
+++ coupon/coupon-api/src/main/kotlin/com.coupon/CouponServerApplication.kt
@@ -3,9 +3,10 @@ package com.coupon
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration
 
 @ConfigurationPropertiesScan
-@SpringBootApplication
+@SpringBootApplication(exclude = [UserDetailsServiceAutoConfiguration::class]) // TODO: Spring Security 적용 후 제거
 class CouponServerApplication
 
 fun main(args: Array<String>) {
